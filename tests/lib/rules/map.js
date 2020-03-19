8a8590a60c22392e4fc4876516af000d3043f36b
MoksS <MoksShuv@yandex.ru>
2020-03-19 14:32:35 +0300

    remove comments

--- tests/lib/rules/map.js
+++ tests/lib/rules/map.js
@@ -28,62 +28,62 @@ ruleTester.run("lodash-to-native", rule, {
     {
       code: "[1, 2, 3].map(fn)"
     },
-    // {
-    //   code: "_.map({a: 1, b: 2, c: 3}, fn)"
-    // },
-    // {
-    //   code: "(new Array(1,2,3,4,5)).map(fn)"
-    // },
-    // {
-    //   code: `
-    //     var arr = "string".split("");
-    //     arr.map(fn)`
-    // },
-    // {
-    //   code: `
-    //     arr = [1,2,3,4];
-    //     arr.map(fn);
-    //   `
-    // },
-    // {
-    //   code: `
-    //     obj = {a:1, b:2, c:3}
-    //     _.map(obj, fn)
-    //   `
-    // }
+    {
+      code: "_.map({a: 1, b: 2, c: 3}, fn)"
+    },
+    {
+      code: "(new Array(1,2,3,4,5)).map(fn)"
+    },
+    {
+      code: `
+        var arr = "string".split("");
+        arr.map(fn)`
+    },
+    {
+      code: `
+        arr = [1,2,3,4];
+        arr.map(fn);
+      `
+    },
+    {
+      code: `
+        obj = {a:1, b:2, c:3}
+        _.map(obj, fn)
+      `
+    }
   ],
 
   invalid: [
-    // {
-    //   code: "_.map([1, 2, 3], fn)",
-    //   errors: [error]
-    // },
-    // {
-    //   code: "_.map([{a: 1, b: 2, c: 3}], fn)",
-    //   errors: [error]
-    // },
-    // {
-    //   code: "_.map([], fn)",
-    //   errors: [error]
-    // },
-    // {
-    //   code: "_.map([{a:1, b:2}, {c:3, d:4}, {}], fn)",
-    //   errors: [error]
-    // },
-    // {
-    //   code: `
-    //     arr = [1,2,3,4,5];
-    //     _.map(arr, fn)
+    {
+      code: "_.map([1, 2, 3], fn)",
+      errors: [error]
+    },
+    {
+      code: "_.map([{a: 1, b: 2, c: 3}], fn)",
+      errors: [error]
+    },
+    {
+      code: "_.map([], fn)",
+      errors: [error]
+    },
+    {
+      code: "_.map([{a:1, b:2}, {c:3, d:4}, {}], fn)",
+      errors: [error]
+    },
+    {
+      code: `
+        arr = [1,2,3,4,5];
+        _.map(arr, fn)
 
-    //   `,
-    //   errors: [error]
-    // },
-    // {
-    //   code: `
-    //     _.map(array, fn);
-    //   `,
-    //   errors: [error]
-    // },
+      `,
+      errors: [error]
+    },
+    {
+      code: `
+        _.map(array, fn);
+      `,
+      errors: [error]
+    },
     {
       code: `
       strArr = "string".split("");
